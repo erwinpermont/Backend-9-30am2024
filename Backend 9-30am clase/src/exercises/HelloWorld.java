3a927e2531d0e1f05965d70835c2a7bbb58ef1a1
erwinpermont <166579378+erwinpermont@users.noreply.github.com>
2024-08-22 07:35:48 -0500

    CONDICIONES RANDOM
    CONDICIONES RANDOM

--- Backend 9-30am clase/src/exercises/HelloWorld.java
+++ Backend 9-30am clase/src/exercises/HelloWorld.java
@@ -3,6 +3,33 @@ package exercises;
 public class HelloWorld {
 public static void main(String [] args){
     System.out.println("Hola Mundo");
+
+    int num1 = 3;
+    int num2 = 4;
+
+    int sum = num1 + num2;
+    int rest = num1 - num2;
+    int mult =  num1 * num2;
+    int div = num1 / num2;
+    int nod = num1;
+
+
+
+    System.out.println(sum);
+    System.out.println("rest = " + rest);
+    System.out.println("Multi = " + mult);
+    System.out.println("div = " + div);
+
+    float num3 = 4.0f;
+    int num4 = 4;
+    boolean comp = num3 == num4;
+    System.out.println("Esta comparacion es : " + comp);
+
+
+
+
+
+
 }
 
 
---
+++ Backend 9-30am clase/src/exercises/baloto.java
@@ -0,0 +1,28 @@
+package exercises;
+
+public class baloto {
+    public staric void main(String)[] args){
+    System.out.println(winnerNum1);
+    System.out.println(winnerNum2);
+    System.out.println(winnerNum3);
+    System.out.println(winnerNum4);
+    System.out.println(winnerNum5);
+    System.out.println(winnerNum6);
+
+    if((userNum1==winnnerNum1  ))
+
+    int userNum1 = 30;
+    int userNum2 = 15;
+    int userNum3 = 17;
+    int userNum4 = 43;
+    int userNum5 = 76;
+    int userNum6 = 92;
+
+    int winnerNum = Random.from(1,100);
+    int winnerNum = Random.from(1,100);
+    int winnerNum = Random.from(1,100);
+    int winnerNum = Random.from(1,100 int winnerNum = Random.from(1,100);
+    int winnerNum = Random.from(1,100
+
+
+}
